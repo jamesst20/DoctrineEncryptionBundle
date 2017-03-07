efcd489a6a391198875b5132841a5c40411308fa
James St-Pierre <Jamesst20@gmail.com>
2017-03-07 16:10:28 -0500

    Prise 2

--- SJ/ExampleBundle/SJExampleBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SJ\ExampleBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SJExampleBundle extends Bundle
-{
-}
--- src/SJ/DoctrineEncryptionBundle/SJDoctrineEncryptionBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SJ\DoctrineEncryptionBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SJDoctrineEncryptionBundle extends Bundle
-{
-}
